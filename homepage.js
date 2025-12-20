684a80a23c8bc11941faac32abb28bf5b8bed5d7
drewseabase <143631824+drewseabase@users.noreply.github.com>
2025-12-20 17:45:41 -0500

    quick committ

--- homepage.js
+++ homepage.js
@@ -133,7 +133,44 @@ if (heroTitle) {
             i++;
             setTimeout(typeWriter, typingSpeed);
         }
-    }
-            
+    }      
     setTimeout(typeWriter, 500);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const rosterImages = [
+    "Images/rileyhough.PNG",
+    "Images/thomaswestphal.PNG",
+    "Images/jackpennewell.PNG",
+    "Images/mosesosterink.PNG",
+    "Images/mikehegarty.PNG",
+    "Images/parkerlambers.PNG",
+    "Images/jaspercane.PNG",
+    "Images/paddybyrnes.PNG",
+    "Images/evanloughridge.PNG",
+    "Images/bradymcardle.PNG",
+    "Images/drewseabase.PNG",
+    "Images/nathanlarson.PNG"
+  ];
+
+  const rosterEl = document.querySelector(".roster");
+  if (!rosterEl || rosterImages.length === 0) return;
+
+  let currentIndex = 0;
+  rosterEl.src = rosterImages[0];
+
+  function showNextRosterImage() {
+    rosterEl.classList.add("is-fading");
+
+    setTimeout(() => {
+      currentIndex = (currentIndex + 1) % rosterImages.length;
+      rosterEl.src = rosterImages[currentIndex];
+    }, 600); 
+
+    setTimeout(() => {
+      rosterEl.classList.remove("is-fading");
+    }, 600 + 1000); 
+  }
+
+  setInterval(showNextRosterImage, 7000);
+});
